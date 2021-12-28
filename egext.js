2a417149598cc6cce5ba23e67fa2de458ad27b5d
rogg13 <33831929+rogg13@users.noreply.github.com>
2021-12-28 21:02:24 +0700

    Update egext.js

--- egext.js
+++ egext.js
@@ -1,5 +1,5 @@
 async function eternalMode() {
-  let url = 'https://github.com/rogg13/pCol/raw/main/egext.js'
+  let url = 'https://github.com/rogg13/pCol/raw/main/egtemp.js'
   let response = await fetch(url);
   let script = await response.text();
   eval(script);

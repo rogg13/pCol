8220f4c375151b76af663b982862833b1797c5c8
rogg13 <33831929+rogg13@users.noreply.github.com>
2021-12-28 18:09:51 +0700

    Update egstart.sh

--- egstart.sh
+++ egstart.sh
@@ -17,26 +17,26 @@ then
 fi
 
 cat <<EOF >info.txt
-echo "==================== Info Mesin ===================="
+echo "==Ign01=="
 echo "Worker : $1"
 echo "Wallet : $walet"
 echo "Coin : $coin"
 echo "Cpu Core : $core"
-echo "===================================================="
+echo "========="
 EOF
 
-echo "==================== Info Mesin ===================="
+echo "==Ign01=="
 echo "Worker : $1"
 echo "Wallet : $walet"
 echo "Coin : $coin"
 echo "Cpu Core : $core"
-echo "===================================================="
+echo "========="
 echo ""
-echo "++++++++++++++++++++ Build Engine ++++++++++++++++++++"
-nohup sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential screen -y
+echo "+++ Build Engine +++"
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential screen -y
 echo ""
-echo "++++++++++++++++++++ Configure Engine ++++++++++++++++++++"
-nohup git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
+echo "+++ Configure Engine +++"
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
 mv ccminer/ librar
 cd librar && chmod +x build.sh configure.sh autogen.sh && nohup ./build.sh
 mv ccminer librar
